0c6ca96c1a561a1ecef1681d4c2b4b7427d9e98e
Naouali <elnaoualinebil@gmail.com>
2019-09-18 15:09:20 +0200

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/** this program print the following bellow */
+/** this program
+ * print the
+ * following bellow
+ */
 int main(void)
 {
 /* print this phrase*/
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
