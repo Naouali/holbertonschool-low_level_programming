576e7a61921d4833be8c367df18acca9cb60b56c
Naouali <elnaoualinebil@gmail.com>
2019-09-24 10:35:07 +0200

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,8 +9,10 @@
 int _isalpha(int c)
 {
 
-	if(c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
-	return (1);
-	else
+	if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+{
+		return (1);
+}
+
 	return (0);
 }
