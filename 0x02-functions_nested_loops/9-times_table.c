69846f5cc62b75f9571beded4a977e5d7ac0c0cd
Naouali <elnaoualinebil@gmail.com>
2019-09-24 15:23:22 +0200

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,35 +1,47 @@
 #include "holberton.h"
 
 /**
- * times_table - check the code for Holberton School students.
+ * times_table - prints the 9 times table starting with 0
  *
- * Return: Always 0.
+ * Return: nothing
  */
 
 void times_table(void)
 {
-	int i, a;
-
-	for (i = 0; i < 10; i++)
+int row, column;
+int ones, tens;
+int multiple;
+for (row = 0; row <= 9; row++)
 {
-	for (a = 0; a < 10; a++)
+for (column = 0; column <= 9; column++)
 {
-	int s = i * a;
-
-	if (s / 10 != 0)
+multiple = row * column;
+if (multiple >= 10)
 {
-	_putchar(s / 10 + '0');
+ones = multiple % 10;
+tens = multiple / 10;
+_putchar(' ');
+_putchar(tens + '0');
+_putchar(ones + '0');
 }
-	_putchar(s % 10 + '0');
-
-	if (a != 9)
+else
 {
-	_putchar(',');
-	_putchar(' ');
+if (column != 0)
+{
+_putchar(' ');
+_putchar(' ');
+}
+_putchar(multiple + '0');
+}
+if (column != 9)
+{
+_putchar(',');
+}
+if (column == 9)
+{
+_putchar('\n');
 }
 }
-	_putchar('\n');
+column = 0;
 }
-
 }
-
