33ef8e9d5839021d46e35794e4e4fd9d970890be
Naouali <elnaoualinebil@gmail.com>
2019-09-24 11:46:39 +0200

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "holberton"
+
+/**
+ * _abs - check the code for Holberton School students.
+ *@int: integer
+ * Return: Always 0.
+ */
+
+int _abs(int)
+{
+	int i;
+
+	if (i < 0)
+{
+
+	return (i * -1);
+}
+	if (i > 0)
+{
+	return (i * 1);
+}
+
+}
+
