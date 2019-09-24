8c66b39388424dbd13ca6773c8211d6fc301a44c
Naouali <elnaoualinebil@gmail.com>
2019-09-24 18:59:47 +0200

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * int add(int, int) - check the code for Holberton School students.
+ * @int: integer
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+
+	return (a+b);
+}
