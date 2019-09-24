dada28e39ccef148d1e385d238c21fa39bc1c870
Naouali <elnaoualinebil@gmail.com>
2019-09-24 10:13:45 +0200

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+
+
+{
+
+
+	int A = 0;
+	while (A < 10)
+	{
+	int i;
+	for(i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	_putchar('\n');
+
+	A++;}
+
+}
