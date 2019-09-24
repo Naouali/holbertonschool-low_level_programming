37107cbfae344e9defeacef84f81d5cae69d5d86
Naouali <elnaoualinebil@gmail.com>
2019-09-24 13:18:45 +0200

    mr james bond

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int i, a;
+
+	for (i = 0; i < 24; i++)
+
+{
+		for (a = 0; a < 60; a++)
+{
+		_putchar(i / 10 + '0');
+	        _putchar(i % 10 + '0');
+
+		_putchar(':');
+		_putchar(a / 10 + '0');
+		_putchar(a % 10 + '0');
+		_putchar('\n');
+}
+
+
+}
+}
