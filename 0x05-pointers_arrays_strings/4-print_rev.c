23b394fd97b490d30c5a2a2eafb1136f8dabc19d
Naouali <elnaoualinebil@gmail.com>
2019-10-01 15:09:55 +0200

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+* print_rev - check the code for Holberton School students.
+*@str: string
+*Return : Always 0.
+*/
+
+void print_rev(char *s)
+
+{
+
+        int i;
+
+
+	i = 0;
+        while (s[i] != '\0')
+{
+	i++;
+
+
+
+}
+
+	for (i = i; i >= 0; i--)
+
+	_putchar(s[i]);
+        _putchar('\n');
+
+}
+
+
+
