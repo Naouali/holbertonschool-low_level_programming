57975e3d65329c2c4d1b4acadb1b7eaa0d9b7bb2
Naouali <elnaoualinebil@gmail.com>
2019-10-02 10:32:38 +0200

    array with lengh

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,19 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i, len;
 
 	i = 0;
 	while (str[i] != '\0')
 {
+	i++;
+}
+	len = i;
 
+	for (i = 0; i < len; i++)
+{
+	if ( i % 2 ==0)
 	_putchar(str[i]);
-	i = i + 2;
 
 
 
