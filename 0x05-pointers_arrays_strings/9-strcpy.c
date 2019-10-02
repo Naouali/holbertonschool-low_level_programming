7ba68e0da611f0e84f64467e8176e3e6ce036f9f
Naouali <elnaoualinebil@gmail.com>
2019-10-02 14:17:42 +0200

    copy paste

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - check the code for Holberton School students.
+ *@dest: string
+ *@src: string
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	while (src[i] != '\0')
+{
+	i++;
+}
+	for (j = 0; j <= i; j++)
+	dest[j] = src[j];
+
+	return (dest);
+}
