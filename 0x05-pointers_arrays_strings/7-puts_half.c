cd1984a5ad2cbdec4045eb1b3949003bfa05cc81
Naouali <elnaoualinebil@gmail.com>
2019-10-02 14:35:16 +0200

    len

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,14 +18,16 @@ void puts_half(char *str)
 {
 	i++;
 }
+
+
 	if (i % 2 == 0)
 {
-	for (j = i / 2; j < i; j++)
+	for (j = i / 2; j < i + 1; j++)
 	_putchar(str[j]);
 }
 	else
 {
-	for (n = (i - 1) / 2; n < i; n++)
+	for (n = (i - 1) / 2; n < i + 1; n++)
 		_putchar(str[n]);
 }
 
