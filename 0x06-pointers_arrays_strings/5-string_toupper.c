5b1ec38c2f5552429971cd96d7094fe3ae6f35d7
Naouali <elnaoualinebil@gmail.com>
2019-10-03 19:43:22 +0200

    upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *string_toupper - check the code for Holberton School students.
+ *@s: string
+ * Return: Always 0.
+ */
+
+char *string_toupper(char *s)
+{
+	int i, j;
+
+	while (s[j] != '\0')
+{
+	j++;
+}
+
+	for (i = 0; i < j; i++)
+{
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 32;
+}
+	return (s);
+}
