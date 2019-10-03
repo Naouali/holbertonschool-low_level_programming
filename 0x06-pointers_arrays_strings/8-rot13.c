bfdf5d18d77dbfbeb03ff1f69d940e30f64f6caa
Naouali <elnaoualinebil@gmail.com>
2019-10-03 23:13:05 +0200

    rot

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *rot13(char *s)
+{
+	int i;
+
+
+	for (i = 0;  s[i] != '\0'; i++)
+{
+	if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+{
+	s[i] = s[i] + 13;
+}
+	else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+
+	s[i] = s[i] - 13;
+}
+	return (s);
+}
