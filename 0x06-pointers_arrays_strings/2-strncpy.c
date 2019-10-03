708ef2dc4e52ff9c31edc42d4e64b02847aa92c6
Naouali <elnaoualinebil@gmail.com>
2019-10-03 15:29:08 +0200

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,18 @@ char *_strncpy(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	while (dest[i] != 0)
+	while (src[i] != 0)
 {
 	i++;
 }
+
 	for (j = 0; j < n; j++)
 {
+	if (src[n - 1] != '\0')
+		dest[j] = 0;
+	if (j > i)
+		dest[j] = '\0';
+	else
 	dest[j] = src[j];
 
 }
