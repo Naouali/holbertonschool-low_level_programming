287a05bf02d25a508e721fce072c99e90c8dbf5d
Naouali <elnaoualinebil@gmail.com>
2019-10-03 15:42:37 +0200

    msg

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, box, j, len;
+
+	i = 0;
+	while (a[i] != '\0')
+{
+	i++;
+}
+	len = i / 2;
+	for (j = len ; j < n ; j++)
+{
+	box = a[j];
+	a[j] = a[j];
+	a[j] = box;
+	j++;
+	j--;
+}
+
+}
