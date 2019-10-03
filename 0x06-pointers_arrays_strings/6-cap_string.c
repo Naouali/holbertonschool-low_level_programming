0e78145fd09a837fdff434650993f335c575e10c
Naouali <elnaoualinebil@gmail.com>
2019-10-03 21:40:55 +0200

    ohhh

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalize string
+ * @s: string
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		if (s[j] == '\n' || s[j] == '\t' || s[j] == ' ' || s[j] == '.'
+		    || s[j] == ';' || s[j] == ',' || s[j] == '!' || s[j] == '?'
+		    || s[j] == '"' || s[j] == '(' || s[j] == ')' || s[j] == '{'
+		    || s[j] == '}')
+		{
+			if (s[j + 1] >= 'a' && s[j + 1] <= 'z')
+
+				s[j + 1] -= 32;
+		}
+	}
+	return (s);
+}
