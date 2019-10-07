ebf5de8999dc0424e75d130a13d1a78ebcf0dca7
Naouali <elnaoualinebil@gmail.com>
2019-10-07 20:52:21 +0200

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,11 +11,11 @@ void print_chessboard(char (*a)[8])
 	int i, j = 0;
 
 	while (j < 8)
-{
-	i = 0;
-	while (i < 8)
-	_putchar(a[j][i++]);
-	_putchar('\n');
-	j++;
+	{
+		i = 0;
+		while (i < 8)
+			_putchar(a[j][i++]);
+		_putchar('\n');
+		j++;
 }
 }
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,3 +1,3 @@
-int _putchar(char c)
+int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
