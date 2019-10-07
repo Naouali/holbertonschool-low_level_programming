befda05cc328d16d9db2bea9d987c452cc05bc08
Naouali <elnaoualinebil@gmail.com>
2019-10-07 20:27:24 +0200

    chess table

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,25 +1,17 @@
-#include "holberton.h"
-
+#include"holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-
+ * print_chessboard - prints a chessboard
+ * @a: array
+ * Return: void
+*/
 void print_chessboard(char (*a)[8])
-
 {
+	int i, j;
 
-	char a[8][8] =
+	for (i = 0; i < 8; i++)
 {
-	{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+	for (j = 0; j < 8; j++)
+	_putchar(a[i][j]);
+	_putchar('\n');
 }
-	_putchar(a);
 }
