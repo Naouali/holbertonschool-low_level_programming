2d06d1397e5ce5f06c8e1a555c432742b580fe14
Naouali <elnaoualinebil@gmail.com>
2019-10-13 17:25:36 +0200

    test

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,16 +3,17 @@
 
 /**
  * main - function that print a new name without recompiling in the shell
- *
- * Reaturn : is a zero
+ *@argc: count of the argument
+ *@argv: array
+ * Return: Always a 0.
  */
 
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 
 {
 	if (argc > 1)
-	printf("%s", argv[0]);
+	printf("%s\n", argv[0]);
 
 
 	return (0);
