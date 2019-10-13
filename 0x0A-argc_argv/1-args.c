b657b8c470f9c5465673ba5a9b1f07525428b7d3
Naouali <elnaoualinebil@gmail.com>
2019-10-13 17:37:01 +0200

    number of argument passed to main

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints it's number of arguments
+ *@argc: int
+ *@argv: array
+ *Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+
+{
+	printf("%d\n", argc);
+	return (0);
+}
