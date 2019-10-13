09944248a1f555bdb03811b361ee40c810857414
Naouali <elnaoualinebil@gmail.com>
2019-10-13 14:33:24 +0200

    static librarie

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,3 +1,16 @@
-void _puts(char *s)
+#include "holberton.h"
+/**
+ * _puts - print a string
+ * @str: string pointer.
+ */
+void _puts(char *str)
 {
+	int i = 0;
+
+	while ((*(str + i) != '\0'))
+	{
+		_putchar((*(str + i)));
+		i++;
+	}
+	_putchar('\n');
 }
