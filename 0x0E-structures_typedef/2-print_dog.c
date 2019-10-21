28be86d697d16016d42d62bb70ae9e638bef1813
Naouali <elnaoualinebil@gmail.com>
2019-10-21 18:34:58 +0200

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	if (((*d).name == NULL) || ((*d).age == 0) || ((*d).owner == NULL))
+	if (((*d).name == NULL) || ((*d).owner == NULL))
 	printf("(nil)");
 	else
 
