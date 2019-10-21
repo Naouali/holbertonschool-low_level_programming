3e03a24614988a676456b54d91f6ca6550c2cfa8
Naouali <elnaoualinebil@gmail.com>
2019-10-21 15:04:38 +0200

    dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,17 @@
-typedef struct dog {
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - new structure called dog
+ * @name: dog ' s name
+ * @age: dog 's age
+ * @owner: dog's owner name
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
