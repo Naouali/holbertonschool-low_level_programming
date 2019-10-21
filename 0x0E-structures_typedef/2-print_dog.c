ab0ad352edd410deb3faf7a4ee95d24322779a9e
Naouali <elnaoualinebil@gmail.com>
2019-10-21 17:10:47 +0200

    hello django

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *print_dog - function to print elements of a struct
+ *@d: pointer to a struct
+ *Return: NULL or char
+ */
+
+void print_dog(struct dog *d)
+
+{
+	if (d == NULL)
+		return;
+
+	if (((*d).name == NULL) || ((*d).age == 0) || ((*d).owner == NULL))
+	printf("(nil)");
+	else
+
+	printf("Name: %s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner);
+}
