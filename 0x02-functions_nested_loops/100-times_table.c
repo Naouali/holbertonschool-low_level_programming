61e292a2615fc7515a65c44a39b71ec790f6e71e
Naouali <elnaoualinebil@gmail.com>
2019-10-31 23:24:28 +0100

    tables times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,5 @@
+#include <stdio.h>
 #include "holberton.h"
-
-/**
- *  
- * 
- * 
- */    
 
 
 void print_times_table(int n)
@@ -13,44 +8,66 @@ void print_times_table(int n)
     for (i = 0; i <= n; i++)
     {
         for (j = 0; j <= n; j++)
-        if (n <= 15 && n > 0)
         {
+        if (n >= 0 && n < 15)  
+        { 
             if (i * j <= 9)
-            {
-                if ( j != 0)
-                _putchar(' ');
+           {
+             if ( j != 0)   
+                {
+                    _putchar(' ');
+                     _putchar(' ');
+                    _putchar(' ');
+                }
+               
                 _putchar((i * j) + '0');
                 if ( j != n)
-                _putchar(',');
-                if (n * j > 9)
-                _putchar(' ');
-                else {
-                _putchar(' ');
-                _putchar(' ');
-                }
-                if (i * j > 9)
-                _putchar(' ');
-            }
-                else if (i * j > 9)
-         
                 {
-                _putchar((i * j /10) + '0');
-                _putchar((i * j % 10) + '0');
-                if (j != n)
-                _putchar(',');
-                if (i * j > 9)
-                _putchar (' ');
-                else {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                }
+                _putchar(' ');
                 
-        }
-        
-            _putchar('\n');
+                }
+               
+           }
+           else if ( i * j < 100)
+           
+           {
+            if (j != 0)
             
-        
+               {
+                    _putchar(' ');
+                    _putchar(' ');
+               }
+               
+                _putchar(i * j / 10  + '0');
+                _putchar(i * j % 10  + '0');
+                if (j != n)
+                {
+                    _putchar(',');
+                    
+                }
+               
+            }
+            else 
+           {
+            if (j != 0)
+            
+               {
+                    _putchar(' ');
+                    
+               }
+                _putchar(i * j / 100 + '0');
+                int x = i * j % 100;
+                _putchar(x / 10  + '0');
+                _putchar(i * j % 10  + '0');
+                if (j != n)
+                {
+                    _putchar(',');
+                    
+                }
+               
+            }
+        }    
+        }
+        _putchar('\n');
     }
-    _putchar('\n');
 }
