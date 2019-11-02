d03e8313bac917e1c01cd017d3ec8a9692843c76
Naouali <elnaoualinebil@gmail.com>
2019-11-02 14:34:51 +0100

    fubonacci is unsigned

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "holberton.h"
 
 /**
  *main - function to print fibonnacci numbers
@@ -16,12 +15,12 @@ int main(void)
 	a = 0;
 	b = 1;
 
-	for (i = 0; i < 50; i++)
+	for (i = 0; i <= 50; i++)
 	{
 	c = a + b;
 	a = b;
 	b = c;
-	printf("%u ,", c);
+	printf("%u\n", c);
 	}
 	return (0);
 }
