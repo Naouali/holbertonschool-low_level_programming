e1343f2ba402d357679f32b3fd47febce43f51ac
Naouali <elnaoualinebil@gmail.com>
2019-11-02 15:13:31 +0100

    fibonacci 's sum

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 /**
- *main - function to print fibonnacci numbers
+ *main - function to print sum of  fibonnacci numbers
  *
  *Return: 0.
  */
 
 int main(void)
 {
-        long i, a, b;
-        long c = 0;
+	long i, a, b;
+	long c = 0;
 	int sum = 0;
 
-        a = 0;
-        b = 1;
+	a = 0;
+	b = 1;
 
-        for (i = 0; i < 50; i++)
-        {
-        c = a + b;
-        a = b;
-        b = c;
-        if (c % 2 == 0 && c < 4000000)
+	for (i = 0; i < 50; i++)
+	{
+	c = a + b;
+	a = b;
+	b = c;
+	if (c % 2 == 0 && c < 4000000)
 	sum += c;
-        }
-        printf("%d\n", sum);
-        return (0);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
 
