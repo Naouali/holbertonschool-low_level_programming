1b2f4e270db9d73969bab127eca34d5b3935e87f
Naouali <elnaoualinebil@gmail.com>
2019-11-14 20:39:44 +0100

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,10 +8,26 @@
  */
 
 int _atoi(char *s)
-{
-	int i;
 
-	for (i  = 0; i != '\0'; i++)
-		if (*i != 4)
-		s[i] = i;
+{
+    int res = 0;
+    int i = 0;
+    int sign = 1;
+    int j ;
+
+    while ((*(s + i) <'0' || *(s + i) < '9') && (*(s + i) != '\0'))
+    {
+        if (*(s + i) == '-')
+        {
+            sign = sign * -1;
+            i++;
+        }
+    }
+    j = 0;
+    while (*(s + j) >= '0' && *(s + j) <= '9')
+    {
+        res = sign * res * 10 + s[j] - '0';
+        j++;
+    }
+    return (res);
 }
