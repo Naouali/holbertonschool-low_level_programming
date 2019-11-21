a5e00673241b7939126b62a5513a6567320ab34b
Naouali <elnaoualinebil@gmail.com>
2019-11-21 15:18:20 +0100

    get value of bit

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,4 +1,9 @@
+#ifndef _HEAD_
+#define _HEAD_
+
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
+
+#endif
