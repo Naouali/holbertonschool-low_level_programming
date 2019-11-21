e4a6762bb52784edd87657f111995c5aceb9efd5
Naouali <elnaoualinebil@gmail.com>
2019-11-21 14:27:23 +0100

    binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,15 +10,14 @@
 
 void print_binary(unsigned long int n)
 {
-	
 
 	if (n <= 1)
 		_putchar(n + '0');
-	else	
-	while(n)
+	else
+	while (n)
 	{
 		_putchar((n & 1) + '0');
 		n = n >> 1;
 	}
 }
-	
+
