858c128d6dacf064d2b43daeec3bb341a99c6ba6
Naouali <elnaoualinebil@gmail.com>
2019-11-21 15:20:28 +0100

    get value of bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (-1);
 	if (n == 0)
 		return (0);
-	if ((n >> index) & 0)
+	if ((n >> index) & 1)
 		return (1);
 	else
 		return (0);
