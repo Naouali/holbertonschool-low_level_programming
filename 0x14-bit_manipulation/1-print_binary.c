1ed9d7f8f6c9545d656a7bc21fc78351dd3c6ef4
Naouali <elnaoualinebil@gmail.com>
2019-11-21 14:25:28 +0100

    binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_binary - function to print binary
+ * @n: integer
+ * return: binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	
+
+	if (n <= 1)
+		_putchar(n + '0');
+	else	
+	while(n)
+	{
+		_putchar((n & 1) + '0');
+		n = n >> 1;
+	}
+}
+	
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,1 +1,3 @@
+int _putchar(char c);
+void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
