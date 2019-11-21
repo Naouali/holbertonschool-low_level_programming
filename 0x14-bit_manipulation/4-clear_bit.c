ff5a9d398add5b6903f66fec15037d9f87bab08a
Naouali <elnaoualinebil@gmail.com>
2019-11-21 16:25:24 +0100

    clear a bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,20 +1,22 @@
+
+
 #include "holberton.h"
 #include <stdlib.h>
 
 /**
- * clear_bit - function to set a bit at a given index
+ * clear_bit - function to clear a bit at a given index
  * @n: number to be modified
- * @index: index in which bits are going to be set
+ * @index: index in which bits are going to be cleard
  * Return: 1 on succes -1 on failure
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	if (index > 32)
+		return (-1);
 
-        if (index > 32)
-                return (-1);
-
-        return (*n |= 0 << index);
+	*n &=~ (1 << index);
+	return (1);
 }
 
 
