e6c82cc3105fa0bd954f0a2449b506743a03c3d4
naouali <elnaoualinebil@gmail.com>
2020-03-12 00:46:23 +0100

    primes

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -27,5 +27,33 @@ int main(void)
 	}
 	return 0;
 	}
+#include <stdio.h>
+#include "prime.h"
+
+/**
+ *
+ *
+ */
+int is_prime(int n)
+{
+        int i, c = 0;
+
+
+        for (i = 1; i <= n; i++)
+        {
+                if (n % i == 0)
+                {
+                        c++;
+                }
+        }
+
+        if (c == 2)
+        {
+                return n;
+        }
+
+        else
+                return 0;
+}
 
 
