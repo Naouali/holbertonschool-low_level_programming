4ab582b8d741179ddac799b721d06a18c95568ee
naouali <elnaoualinebil@gmail.com>
2020-03-31 03:56:03 +0100

    change

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,17 +17,18 @@ int create_file(const char *filename, char *text_content)
 {
   //int empty;
   // empty = open(filename, O_CREAT | O_RDWR);
+  int fd;
   
   if (filename == NULL)
     return (-1);
   if (text_content == NULL)
     return 1;
 
-  int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+  fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
   if (fd == -1)
     {
       //printf("failed to creat a file");
-    return -1;
+      return (-1);
     }
   write(fd, text_content, strlen(text_content));
 
