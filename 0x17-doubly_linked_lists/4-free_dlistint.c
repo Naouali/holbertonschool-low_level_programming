748b1b7f2eb84e0e08d66e67d2710e689f00207c
Naouali <elnaoualinebil@gmail.com>
2020-04-13 22:01:09 +0100

    free

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - function to free alist
+ * @head: head of the list
+ * Return; nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *store, *p;
+
+	p = head;
+	while (p)
+	{
+		store = p->next;
+		free(p);
+		p = store;
+	}
+}
