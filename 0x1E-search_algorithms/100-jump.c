15d70816458566ecc2735650a9bb570811c2d3de
Naouali <elnaoualinebil@gmail.com>
2020-04-15 10:03:05 +0100

    needs revisio

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * jump_search - function to perform a jump search
+ * @array: array to be searched
+ * @size: size of the array
+ * value: value to be looking for
+ * Return: first index of the value or -1
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t m = sqrt(size);
+	size_t i = 0;
+	size_t jump = 0;
+
+	while (i <= size)
+	{
+		if (value == array[i])
+			return (i);
+		else if (value == array[m])
+			return (m);
+		if (value > array[i] && value > array[m])
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+		{
+			jump++;
+			i = i + m;
+			m = m * jump;
+		}
+		if (value > array[i] && value < array[m])
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n", i, m);
+			i = i + 1;
+		}
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,2 +1,4 @@
+#include <stddef.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
