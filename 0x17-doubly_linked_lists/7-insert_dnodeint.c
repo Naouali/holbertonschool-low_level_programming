54b29540306414735857535d23426556cee3c1d7
Naouali <elnaoualinebil@gmail.com>
2020-04-15 13:27:36 +0100

    insert at nth position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - function to insert node
+ * @h: head of the list
+ * @idx: index of the node to add
+ * @n: data to be hold by the node
+ * Return: pointer to the new node or NULL
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 1;
+	dlistint_t *p, *nx;
+	dlistint_t *node = malloc(sizeof(dlistint_t));
+
+	if (node == NULL)
+		return (NULL);
+
+
+	p = *h;
+	while (i < idx)
+	{
+		p = p->next;
+		i++;
+	}
+	nx = p->next;
+	p->next = node;
+	node->prev = p;
+	node->next = nx;
+	node->n = n;
+	nx->prev = node;
+
+
+	return (node);
+}
+
