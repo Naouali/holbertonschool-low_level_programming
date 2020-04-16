70a3bbd5fbc56bbfac684e7ae4eb1114558b6ac3
Naouali <elnaoualinebil@gmail.com>
2020-04-16 16:53:20 +0100

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,4 +1,6 @@
-#include <stddef.h>
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+#endif
